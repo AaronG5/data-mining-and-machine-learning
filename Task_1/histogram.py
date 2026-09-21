7de90a785edb0d3a27547f7eeb09d1c7c4e8af9c
Aaron Gandžumian <aaron.gandzumian@gmail.com>
2026-09-22 00:56:22 +0300

    Hide normal curve

--- Task_1/histogram.py
+++ Task_1/histogram.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import os
 
-def plot_distribution(values: pd.Series, title: str, out_dir: str, save_as_png: bool=True, show: bool=False, bins=30) -> None:
+def plot_distribution(values: pd.Series, title: str, out_dir: str, save_as_png: bool=True, show: bool=False, bins=10) -> None:
    values = pd.to_numeric(values, errors='coerce').dropna()
    mean = values.mean()
    std = values.std()
@@ -11,17 +11,21 @@ def plot_distribution(values: pd.Series, title: str, out_dir: str, save_as_png: 
 
    plt.figure(figsize=(9, 5))
 
-   plt.hist(values, bins, density=True, alpha=0.5, color='steelblue', edgecolor='black')
+   _, edges, _ = plt.hist(values, bins, density=False, alpha=0.5, color='steelblue', edgecolor='black')
+   # bin_width = edges[1] - edges[0]
 
-   if std > 0:
-      x = np.linspace(values.min(), values.max(), 500)
-      pdf = np.exp(-0.5 * ((x - mean) / std) ** 2) / (std * np.sqrt(2 * np.pi))
-      plt.plot(x, pdf, color='black', lw=2)
+   plt.xticks(edges, [f'{e}' for e in edges])
+   plt.xlim((values.min(), values.max()))
+
+   # if std > 0:
+   #    x = np.linspace(values.min(), values.max(), 500)
+   #    pdf = np.exp(-0.5 * ((x - mean) / std) ** 2) / (std * np.sqrt(2 * np.pi))
+   #    plt.plot(x, pdf * len(values) * bin_width, color='black', lw=2, label='Normalioji kreivė')
 
    plt.axvline(mean, color='red', label=f'Vidurkis = {mean:.2f}')
    plt.axvline(median, color='green', label=f'Mediana = {median:.2f}')
    plt.axvline(q1, color='orange', label=f'1-as kvartilis = {q1:.2f}')
-   plt.axvline(q3, color='purple', label=f'3-as kvartilis = {q3:.2f}')
+   plt.axvline(q3, color='blue', label=f'3-as kvartilis = {q3:.2f}')
 
    plt.title(title)
    plt.xlabel('Įgyjamų reikšmių intervalai')
