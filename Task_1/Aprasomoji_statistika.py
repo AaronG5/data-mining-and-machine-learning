5f4152305b9f1aaf1f61ddcf19fa8b6a0a82cf88
Aaron Gandžumian <aaron.gandzumian@gmail.com>
2026-09-21 21:40:29 +0300

    Refactor code for readability and reuse

--- Task_1/Aprasomoji_statistika.py
+++ Task_1/Aprasomoji_statistika.py
@@ -1,16 +1,13 @@
 import pandas as pd
 import dataframe_image as dfi
+import matplotlib.pyplot as plt
 
 FILE = 'A27/A27.csv'
 CLASSES = ['Bumps', 'Other_Faults']
 FEATURES = ['Log_X_Index', 'Log_Y_Index', 'Empty_Index', 'Square_Index', 'Length_of_Conveyer',
              'Steel_Plate_Thickness', 'Edges_Index', 'Orientation_Index', 'LogOfAreas', 'Luminosity_Index']
 
-df = pd.read_csv(FILE, skipinitialspace=True)
-df.columns = df.columns.str.strip()
-df['class'] = df['class'].str.strip()
-
-def descriptive_statistics(part: pd.DataFrame):
+def descriptive_statistics(part: pd.DataFrame) -> pd.DataFrame:
    classifiers = part[FEATURES]
    rows = []
 
@@ -31,14 +28,14 @@ def descriptive_statistics(part: pd.DataFrame):
 
    return pd.DataFrame(rows)
 
-def export_table_png(table, filename):
+def export_table_png(table: pd.DataFrame, filename: str) -> None:
    styled = table.style.format(precision=4).hide(axis='index')
    dfi.export(styled, filename + '.png')
 
-def export_table_csv(table, filename):
+def export_table_csv(table: pd.DataFrame, filename: str) -> None:
    table.to_csv(filename + '.csv', index=False, float_format='%.4f')
 
-def check_for_duplicates(data: pd.DataFrame):
+def check_for_duplicates(data: pd.DataFrame) -> pd.DataFrame:
    dupes = data.duplicated(keep='first')
 
    print(f'Total rows: {len(data)}')
@@ -46,20 +43,28 @@ def check_for_duplicates(data: pd.DataFrame):
 
    return data[dupes]
 
-# duplicates = check_for_duplicates(df)
-# print(duplicates)
 
-df = df.drop_duplicates().reset_index(drop=True)
+def main():
+   df = pd.read_csv(FILE, skipinitialspace=True)
+   df.columns = df.columns.str.strip()
+   df['class'] = df['class'].str.strip()
 
-filename = 'bendra_aprasomoji_statistika'
-table = descriptive_statistics(df)
-export_table_csv(table, filename)
+   # duplicates = check_for_duplicates(df)
+   # print(duplicates)
 
-for class_name in CLASSES:
-   filename = class_name + '_aprasomoji_statistika'
-   part = df[df['class'] == class_name]
+   df = df.drop_duplicates().reset_index(drop=True)
 
-   table = descriptive_statistics(part)
-
-   # export_table_png(table, filename)
+   filename = 'bendra_aprasomoji_statistika'
+   table = descriptive_statistics(df)
    export_table_csv(table, filename)
+
+   for class_name in CLASSES:
+      filename = class_name + '_aprasomoji_statistika'
+      part = df[df['class'] == class_name]
+
+      table = descriptive_statistics(part)
+
+      # export_table_png(table, filename)
+      export_table_csv(table, filename)
+
+main()
