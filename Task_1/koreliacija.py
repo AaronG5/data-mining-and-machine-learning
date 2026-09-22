ac8ab8b6602e28765d5be05f173bf0c8ac9b7a31
Augustė Tumaitė <120650538+augustetum@users.noreply.github.com>
2026-09-22 21:08:16 +0300

    Refactor file paths to use BASE_DIR

--- Task_1/koreliacija.py
+++ Task_1/koreliacija.py
@@ -1,7 +1,9 @@
+import os
 import pandas as pd
 from scipy import stats
 
-FILE = 'A27_svarus.csv'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FILE = os.path.join(BASE_DIR, 'A27_svarus.csv')
 FEATURES = ['Log_X_Index', 'Log_Y_Index', 'Empty_Index', 'Square_Index', 'Length_of_Conveyer',
             'Steel_Plate_Thickness', 'Edges_Index', 'Orientation_Index', 'LogOfAreas', 'Luminosity_Index']
 
@@ -11,10 +13,10 @@ def main():
    df.columns = df.columns.str.strip()
 
    rho = df[FEATURES].corr(method='spearman')
-   rho.to_csv('koreliacija_spearman.csv', float_format='%.4f')
+   rho.to_csv(os.path.join(BASE_DIR, 'koreliacija_spearman.csv'), float_format='%.4f')
 
    p = pd.DataFrame(stats.spearmanr(df[FEATURES])[1], index=FEATURES, columns=FEATURES)
-   p.to_csv('koreliacija_p.csv', float_format='%.3g')
+   p.to_csv(os.path.join(BASE_DIR, 'koreliacija_p.csv'), float_format='%.3g')
 
 
 main()
